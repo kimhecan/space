30a00e539d59b15323f346ac043b8756b9c1f1ff
kimheechan <kimhecan@rainist.com>
2024-04-29 14:04:09 +0900

    Production과 development의 두 환경 분리

---
+++ @types/global.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly DB_USERNAME: string;
+    readonly DB_PASSWORD: string;
+    readonly DB_DATABASE: string;
+    readonly SESSION_SECRET: string;
+    readonly JWT_SECRET: string;
+    readonly NODE_ENV: 'development' | 'production';
+    readonly LOGGING: boolean;
+    readonly DB_HOST: string;
+  }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
